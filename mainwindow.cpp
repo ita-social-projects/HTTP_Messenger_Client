ca05ac5cfe0d172c00110346e18a9ff056a11fa4
Hryshchuk_Artem <72103688+Morituri67@users.noreply.github.com>
2021-08-29 12:39:55 +0300

    Add layouts
    Made objects size to be adjustable

---
+++ mainwindow.cpp
@@ -0,0 +1,112 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , main_w(new Ui::MainWindow)
+{
+    connect(&log_w, SIGNAL(LoginButton_signal()), this, SLOT(logging()));
+    connect(&log_w, SIGNAL(SignupButton_signal()), this, SLOT(SignupWindow_open()));
+    connect(&sign_w, SIGNAL(LoginButton_signal()), this, SLOT(LoginWindow_open()));
+    connect(&sign_w, SIGNAL(SignupButton_signal()), this, SLOT(registration()));
+
+    main_w->setupUi(this);
+    main_w->ShowOrHideProfileButton->setCheckable(true);
+    main_w->ShowOrHideProfileButton->setChecked(true);
+}
+
+MainWindow::~MainWindow()
+{
+    delete main_w;
+}
+
+void MainWindow::display()
+{
+    log_w.show();
+}
+
+void MainWindow::close_window()
+{
+    this->close();
+    main_w->UserName->clear();
+    main_w->ChatName->clear();
+    main_w->EnterMessage->clear();
+    main_w->SearchUser->clear();
+    main_w->UserList_2->clear();
+    main_w->Messages->clear();
+}
+
+void MainWindow::LoginWindow_open()
+{
+    log_w.show();
+    sign_w.close_window();
+}
+
+void MainWindow::SignupWindow_open()
+{
+    sign_w.show();
+    log_w.close_window();
+}
+
+void MainWindow::logging()
+{
+    // request to server
+    QString server_answer = "Success";
+    if(server_answer == "Success")
+    {
+        main_w->UserName->setText(log_w.GetLogin());
+        this->show();
+        log_w.close_window();
+    }
+}
+
+void MainWindow::registration()
+{
+    QString pass1 = sign_w.GetPassword();
+    QString pass2 = sign_w.GetPasswordAgain();
+    if(pass1 != pass2)
+    {
+        QMessageBox::warning(this, "ERROR","Invalid login or password!");
+
+    }
+    // request to server
+    QString server_answer = "Success";
+    if(server_answer == "Success")
+    {
+        log_w.show();
+        sign_w.close_window();
+    }
+}
+
+
+void MainWindow::on_SendButton_clicked()
+{
+
+}
+
+void MainWindow::on_SearchUserButton_clicked()
+{
+
+}
+
+void MainWindow::on_ExitButton_clicked()
+{
+    log_w.show();
+    this->close_window();
+}
+
+
+void MainWindow::on_ShowOrHideProfileButton_clicked()
+{
+    if(main_w->ShowOrHideProfileButton->isChecked()){
+        main_w->profileWidget_2->show();
+    }
+    else
+    {
+        main_w->profileWidget_2->hide();
+    }
+}
+
+
+
+
