f7a47263ade26d6eaf5bef3535cf2d3d0223b9f0
Roman <46456972+RomanKasko@users.noreply.github.com>
2021-09-14 14:36:40 +0300

    CLV-77 SingUp screen. Made signUp design, read data from ui, validation

--- messenger/signupinfo.cpp
+++ messenger/signupinfo.cpp
@@ -53,5 +53,30 @@ void SignUpInfo::unpack()
 
 bool SignUpInfo::send()
 {
+    /*QNetworkAccessManager *mgr = new QNetworkAccessManager();
+        const QUrl url(QStringLiteral("http://google.com/api"));
+        QNetworkRequest request(url);
+        request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+        QJsonObject obj;
+        obj["login"] = "bel.vita";
+        obj["password"] = "12345";
+        QJsonDocument doc(obj);
+        QByteArray data = doc.toJson();
+        // or
+        // QByteArray data("{\"key1\":\"value1\",\"key2\":\"value2\"}");
+        QNetworkReply *reply = mgr->post(request, data);
+        QObject::connect(reply, &QNetworkReply::finished, [=](){
+            if(reply->error() == QNetworkReply::NoError){
+                QString contents = QString::fromUtf8(reply->readAll());
+                qDebug() << contents;
+                qWarning("\n no error \n");
+            }
+            else{
+                QString err = reply->errorString();
+                qDebug() << err;
+                qWarning("\n error: \n");
+            }
+            reply->deleteLater();
+        });*/
     return true;
 }
