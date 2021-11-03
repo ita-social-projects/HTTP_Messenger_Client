e85dc7fa0af6d1316d8b8bd0a9d95481d7264606
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-11-03 22:58:10 +0200

    Added file for Jenkins build

---
+++ messenger/qtbuild.bat
@@ -0,0 +1,3 @@
+dir & "C:\Qt\6.1.2\msvc2019_64\bin\qmake.exe" -config debug & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64\nmake.exe" -f makefile debug
+timeout /t 30
+
